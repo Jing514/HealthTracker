ee7872e57eefc4b0897042a43ff5ccc39f0c2a41
Jing514 <jing.linb2026@stu.tes.tp.edu.tw>
2025-08-12 15:40:01 +0800

    Goals tab start

---
+++ lib/goals_folder/goals_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:project/goals_folder/widget/goals_widget.dart';
+
+class GoalsScreen extends StatelessWidget {
+  final DateTime date;
+
+  const GoalsScreen({
+    super.key,
+    required this.date,
+  });

1f3671125457f51512ea2ecc6a3681067dd96fbf
Jing514 <jing.linb2026@stu.tes.tp.edu.tw>
2025-08-12 15:36:10 +0800

    Setup Firebase database

--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,34 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) =>
+      MaterialApp(title: 'Health Tracker', theme: ThemeData(primarySwatch: Colors.blue), home: const FitnessApp());
+}
+
+class FitnessApp extends StatefulWidget {
+  const FitnessApp({super.key});
+  @override
+  State<FitnessApp> createState() => _FitnessAppState();
+}
+
+class _FitnessAppState extends State<FitnessApp> {
+  final Map<String, DayData> dataMap = {};
+  DateTime selectedDate = DateTime.now();
+  int tabIndex = 3;
+  Color tabColor = Colors.blue;
+  bool isDisableSelectDate = false;
+  bool isDisableBottomNav = true;
+
+
+  String formatDate(DateTime d) => '${d.year}-${d.month.toString().padLeft(2, '0')}-${d.day.toString().padLeft(2, '0')}';
+
+  DayData getDay(DateTime d) {
+    final k = formatDate(d);
+    dataMap[k] ??= DayData(dailyGoals: [], calorieIntake: 0, waterIntake: 0, weight: 0);
+    return dataMap[k]!;
+  }
+
